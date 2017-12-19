b51494bece6cf1d5b032ea06720d5c0493f0429b
Jose Deuteros <josewbmx@gmail.com>
2017-12-18 21:33:40 -0600

    Se agregaron los links de las redes

--- wp-content/themes/TorresP/contenidos/home/5-footer.php
+++ wp-content/themes/TorresP/contenidos/home/5-footer.php
@@ -11,27 +11,27 @@
 	<div class="Bloques">
 		<div class="redes_footer">
 			<div class="redes">
-				<a href="#" target="_blank"> 
+				<a href="https://www.facebook.com/CarlosTorresPRD/" target="_blank"> 
 					 <div class="facebook"></div>
 				</a>
 			</div>
 
 			<div class="redes">
-				<a href="#" target="_blank"> 
+				<a href="https://twitter.com/CarloTorresP?lang=es" target="_blank"> 
 					 <div class="twitter"></div>
 				</a>
 			</div>
 
 
 			<div class="redes">
-				<a href="#" target="_blank"> 
+				<a href="https://www.instagram.com/carlostorresprd/?hl=es-la" target="_blank"> 
 					 <div class="instagram"></div>
 				</a>
 			</div>
 
 
 			<div class="redes">
-				<a href="#" target="_blank"> 
+				<a href="https://www.youtube.com/channel/UCy1tFXyVXxtuvUlq1KrtvkA" target="_blank"> 
 					 <div class="youtube"></div>
 				</a>
 			</div>
