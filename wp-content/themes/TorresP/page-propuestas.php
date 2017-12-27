5b50cd781ef600489adecbaf56cea0c9d119c014
Jose Deuteros <josewbmx@gmail.com>
2017-12-26 22:30:19 -0600

    Se creo la página de propuestas

---
+++ wp-content/themes/TorresP/page-propuestas.php
@@ -0,0 +1,40 @@
+<?php /* Template Name: propuestas */  ?>
+
+<?php get_header(); ?>
+
+
+
+<div class="main">
+
+
+
+<!-- HEADER -->
+<?php echo get_template_part("contenidos/home/1-header"); ?>
+
+<!-- VIDEOS LOOPS-->
+ <?php echo get_template_part("contenidos/propuestas/propuestas"); ?>
+
+
+ 
+
+ <!-- Footer -->
+<?php echo get_template_part("contenidos/home/5-footer"); ?>
+
+
+
+</div> <!-- Main -->
+
+
+
+
+ 
+
+
+
+
+ 
+
+
+
+<?php get_footer(); ?>
+ 
